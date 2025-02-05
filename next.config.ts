8de3b7be30d39f641004fe7e87901fa06f55f0a1
hom-cv <chaithatvanasrisawasd@gmail.com>
2025-02-06 00:09:46 +0700

    Fix website metadata

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? "/bareshells/" : "",
-  basePath: isProd ? "/bareshells" : "",
   output: "export",
 };
 
