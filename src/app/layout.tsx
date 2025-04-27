cf910a857f044bef2d8c52e1dd8277501d9d761a
hom-cv <chaithatvanasrisawasd@gmail.com>
2025-04-27 20:20:52 +0700

    add image to opengraph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,22 @@ export const metadata: Metadata = {
     icon: [{ url: "/favicon.png" }],
     apple: [{ url: "/favicon.png" }],
   },
+  openGraph: {
+    title: "Bareshells",
+    description:
+      "Bareshells is a multidisciplinary design studio that celebrates the harmony of simplicity and substance.",
+    url: "https://bareshells.com", // your real domain here
+    siteName: "Bareshells",
+    images: [
+      {
+        url: "https://bareshells.com/scaffolding.PNG", // must be absolute URL!
+        width: 1200,
+        height: 630,
+        alt: "Bareshells Studio",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
