1e207ece64e9dcb78b5b18ef6e603f02bceef2d5
hom-cv <chaithatvanasrisawasd@gmail.com>
2025-04-28 21:51:22 +0700

    add shop placeholder

--- src/app/(shop-sections)/shop/page.tsx
+++ src/app/(shop-sections)/shop/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import SocialLinks from "@/components/SocialLinks";
+import Image from "next/image";
 
 export default function ShopPage() {
   return (
     <div className="w-full flex-grow flex flex-col bg-white">
-      <div className="flex-grow flex flex-col justify-center items-center  "></div>
+      <div className="flex-grow flex flex-col justify-center items-center  ">
+        <Image
+          src="/lounge-sketch.jpg"
+          alt="exhibition"
+          width={340}
+          height={340}
+        />
+      </div>
       <div className="mt-auto">
         <SocialLinks />
       </div>
