f2559c09a324ab975faf923abb596776376df216
hom-cv <chaithatvanasrisawasd@gmail.com>
2025-04-28 21:48:45 +0700

    remove bareshells logo

--- src/app/(shop-sections)/shop/page.tsx
+++ src/app/(shop-sections)/shop/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 
 import SocialLinks from "@/components/SocialLinks";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function ShopPage() {
   return (
     <div className="w-full flex-grow flex flex-col bg-white">
-      <div className="flex-grow flex flex-col justify-center items-center  ">
-        <Link href="/">
-          <Image
-            src="/Bareshells-Thin.svg"
-            alt="BARESHELLS"
-            width={100}
-            height={16}
-            className="w-auto h-3 object-contain mb-0.5"
-          />
-        </Link>
-      </div>
+      <div className="flex-grow flex flex-col justify-center items-center  "></div>
       <div className="mt-auto">
         <SocialLinks />
       </div>
