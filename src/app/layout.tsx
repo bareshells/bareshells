d15ea89a8db0ee3685385c30b6019a602244fd71
hom-cv <chaithatvanasrisawasd@gmail.com>
2025-11-21 00:48:49 +0700

    fix: Remove metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,22 +5,14 @@ export const metadata: Metadata = {
   title: "Bareshells",
   applicationName: "Bareshells",
   description:
-    "Bareshells is a design studio dedicated to crafting refined furniture and objects.",
+    "",
   viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
   openGraph: {
     title: "Bareshells",
     description:
-      "Bareshells is a design studio dedicated to crafting refined furniture and objects.",
+      "",
     url: "https://bareshells.com", // your real domain here
     siteName: "Bareshells",
-    images: [
-      {
-        url: "https://bareshells.com/background.png", // must be absolute URL!
-        width: 1200,
-        height: 630,
-        alt: "Bareshells Studio",
-      },
-    ],
     type: "website",
   },
 };
